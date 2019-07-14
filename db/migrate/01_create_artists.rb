574d0aae81889a1206de63c04d0a754890f79e4f
Jenna Ransom <jenna.ransom@gmail.com>
2019-07-14 21:34:33 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -1,14 +1,7 @@
 class CreateArtists < ActiveRecord::Migration
-  def up
-  end
- 
-  def down
-  end
-  
   def change
-    create_table :artists do 
-      |t|
-            t.string :name
+    create_table :artists do |t|
+      t.string :name
       t.string :genre
       t.integer :age
       t.string :hometown
